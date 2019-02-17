91f22ee7651bda2667f30b43553b695f1f92544a
ACayet <arthurcayet@hotmail.fr>
2019-02-17 23:15:05 +0100

    risitas command finished wip api call message display

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # PsyBot
+
+## Prérequis
+
+* [NodeJS](https://nodejs.org/en) - Contient le gestionnaire de dependence npm
+* [Discord.js](https://discord.js.org/#/docs/main/stable/general/welcome) - Librairie node permettant de manipuler l'api discord
+* [Winston](https://www.npmjs.com/package/winston) - Un logger sympa
+* [dotenv](https://www.npmjs.com/package/dotenv) - Permet de charger les fichier .env et d'utiliser les variable d'environnement
+* [request-promise](https://www.npmjs.com/package/request-promise) - Requetes http asynchrone pour les differentes apis
+
+## Variables d'environement
+
+* PREFIX - Prefixe des commandes dans tout le bot
--- commands/risitas.js
+++ commands/risitas.js
@@ -4,6 +4,7 @@
  */
 const Discord = require('discord.js');
 require('dotenv').config()
+const rp = require('request-promise');
 
 /**
  * Show a random risitas
@@ -11,6 +12,39 @@ require('dotenv').config()
  * @param {Array[String]} args 
  */
 const risitas = (message, args) => {
+    
+    let options;
+
+    options = {
+        method: 'POST',
+        uri: process.env.RISIBANKAPI,
+        body: {
+            search: args[0]
+        },
+        json: true
+    };
+    rp(options)
+        .then(function (body) {
+            //console.log(body);
+            if(body.stickers.length > 0) {
+                let randomPic = Math.floor(Math.random() * Math.floor(body.stickers.length));
+
+                if(body.stickers[randomPic].risibank_link) {
+                    message.channel.send("Un risitas aleatoire pour "+ args[0], {
+                        file: body.stickers[randomPic].risibank_link
+                    });
+                } else {
+                    message.channel.send("Aucune photo trouvé pour ta recherche");
+                }
+            } else {
+                message.channel.send("Aucune photo trouvé pour ta recherche");
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+            message.channel.send("Probleme avec l'api, veuillez ressayer");
+        });
+  
 
 }
 
@@ -19,9 +53,9 @@ module.exports = {
     name: "risitas",
     help: {
         desc: "J'affiche des risitas aleatoire depuis risibank",
-        format: ""
+        format: "[mot-clé]"
     },
-    args: false,
+    args: true,
     mentions: false,
     execute: risitas
 };
